4f6526e378a4976fc9ca3b8f99f99c76acafbdab
EunHaSong <shaeun03290@sookmyung.ac.kr>
2026-05-11 19:12:21 +0900

    uiux: 마이페이지 홈바 및 프로필 위젯 생성

--- lib/mypage/view/main.dart
+++ lib/mypage/view/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project_daon/mypage/widget/myProfileWidget.dart';
+import 'package:project_daon/mypage/widget/mypageAppBarWidget.dart';
 import 'package:project_daon/ui/colorStyles.dart';
+import 'package:project_daon/ui/fontStyles.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({super.key});
+
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  String name = '이재현';
+  int level = 2;
+  String disaster = '홍수';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      // appBar: MyPageAppBarWidget(),
+      appBar: MypageAppBarWidget(),
       body: Container(
         width: double.infinity,
         height: double.infinity,
+        padding: EdgeInsets.only(top: 100.0),
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment(0.00, 0.00),
@@ -19,7 +32,11 @@ class MyPage extends StatelessWidget {
             colors: [ColorStyles.main2, ColorStyles.main3],
           ),
         ),
-        child: Center(child: Text('My Page')),
+        child: Column(
+          children: [
+            MyProfileWidget(name: name, level: level, disaster: disaster),
+          ],
+        ),
       ),
     );
   }
